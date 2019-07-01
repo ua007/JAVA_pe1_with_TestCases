78095635c78e92b04ecb2e3df795134fabd04384
ua007 <35341797+ua007@users.noreply.github.com>
2019-07-01 16:48:06 +0530

    Update string_reverse.java

--- src/main/java/com/stackroute/pe1/string_reverse.java
+++ src/main/java/com/stackroute/pe1/string_reverse.java
@@ -1,3 +1,5 @@
+//reverse the input string
+
 package com.stackroute.pe1;
 
 import java.util.Scanner;
@@ -8,13 +10,14 @@ public class string_reverse {
         // write your code here
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter a string");
-        String str = sc.next();
+        String str = sc.next();//input the string
         System.out.println(stringReverse(str));
     }
 
     public static String stringReverse(String str) {
 
         String reverse = "";
+        //reversing the string
         for(int i=str.length()-1; i>=0; i--) {
             reverse = reverse + str.charAt(i);
         }
