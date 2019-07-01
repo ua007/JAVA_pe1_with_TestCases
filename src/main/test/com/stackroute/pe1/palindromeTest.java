334ab4e53b9402716a6c6762124b2abb2caac97a
ua007 <35341797+ua007@users.noreply.github.com>
2019-07-01 16:35:41 +0530

    Update palindromeTest.java

--- src/main/test/com/stackroute/pe1/palindromeTest.java
+++ src/main/test/com/stackroute/pe1/palindromeTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class palindromeTest {
+    //test case for being a palindrome and sum of even digits is greater than 25
     @Test
     public void givenIntegerIsPalindromeSumGreaterThanShouldReturnMessage(){
         //
@@ -14,6 +15,7 @@ public class palindromeTest {
         //assert
         assertEquals("is palindrome and the sum of even numbers is greater than 25",result);
     }
+    //test case for being a palindrome and sum of even digits is less than 25
     @Test
     public void givenIntegerIsPalindromeSumLessThanShouldReturnMessage(){
         palindrome ab=new palindrome();
@@ -23,6 +25,7 @@ public class palindromeTest {
         //assert
         assertEquals("is palindrome and the sum of even numbers is less than 25",result);
     }
+    //test case for not being a palindrome 
     @Test
     public void givenIntegerIsNotPalindromeShouldReturnMessage(){
         palindrome ab=new palindrome();
