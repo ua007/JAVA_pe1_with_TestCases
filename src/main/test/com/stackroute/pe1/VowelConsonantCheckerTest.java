219a13809c1e22547716382b0d818b0e37675c59
ua007 <35341797+ua007@users.noreply.github.com>
2019-07-01 16:23:54 +0530

    Update VowelConsonantCheckerTest.java

--- src/main/test/com/stackroute/pe1/VowelConsonantCheckerTest.java
+++ src/main/test/com/stackroute/pe1/VowelConsonantCheckerTest.java
@@ -4,11 +4,20 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class VowelConsonantCheckerTest {
+    //test case for lowercase character
     @Test
     public void givenStringShoulhReturnVowelOrConsonant(){
         VowelConsonantChecker str=new VowelConsonantChecker();
         String result;
         result=str.vowelConsonantCheck("abab");
+        assertEquals("Vowel Consonant Vowel Consonant ",result);
+    }
+    //test case for uppercase character
+    @Test
+    public void givenStringUpperCaseShoulhReturnVowelOrConsonant(){
+        VowelConsonantChecker str=new VowelConsonantChecker();
+        String result;
+        result=str.vowelConsonantCheck("ABAB");
         assertEquals("Vowel Consonant Vowel Consonant ",result);
     }
 
