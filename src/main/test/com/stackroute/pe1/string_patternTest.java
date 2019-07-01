3207cedabd9650fb448ee434d4fa176b4c4c231b
ua007 <35341797+ua007@users.noreply.github.com>
2019-07-01 16:47:05 +0530

    Update string_patternTest.java

--- src/main/test/com/stackroute/pe1/string_patternTest.java
+++ src/main/test/com/stackroute/pe1/string_patternTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class string_patternTest {
+    //test case for string stackroute and integer 2
     @Test
     public void givenStringShouldReturnStringPattern(){
         string_pattern str=new string_pattern();
