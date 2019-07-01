7f6aa2900d4a241d5e55b7d5b5a954b77fef6c03
ua007 <35341797+ua007@users.noreply.github.com>
2019-07-01 16:52:34 +0530

    Update tom_and_jeeryTest.java

--- src/main/test/com/stackroute/pe1/tom_and_jeeryTest.java
+++ src/main/test/com/stackroute/pe1/tom_and_jeeryTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class tom_and_jeeryTest {
+    //success test cases
+    
+    //test case for jerry
     @Test
     public void givenIntegerIsBetween20And30AndIsEvenShouldReturnJerry(){
         tom_and_jeery tj=new tom_and_jeery();
@@ -11,6 +14,7 @@ public class tom_and_jeeryTest {
         result=tj.tom_and_jerry_check(24);
         assertEquals("Jerry",result);
     }
+    //test case for tom
     @Test
     public void givenIntegerIsBetween20And30AndIsEvenShouldReturnTom(){
         tom_and_jeery tj=new tom_and_jeery();
@@ -18,6 +22,7 @@ public class tom_and_jeeryTest {
         result=tj.tom_and_jerry_check(25);
         assertEquals("Tom",result);
     }
+    //failure test case
     @Test
     public void givenIntegerIsNotBetween20And30ShouldReturnTom(){
         tom_and_jeery tj=new tom_and_jeery();
