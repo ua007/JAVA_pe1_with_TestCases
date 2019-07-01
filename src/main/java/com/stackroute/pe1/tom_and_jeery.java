3e2529a69b90560e4219517744a4e7b58be7abc6
ua007 <35341797+ua007@users.noreply.github.com>
2019-07-01 16:51:26 +0530

    Update tom_and_jeery.java

--- src/main/java/com/stackroute/pe1/tom_and_jeery.java
+++ src/main/java/com/stackroute/pe1/tom_and_jeery.java
@@ -1,3 +1,6 @@
+//if the input integer is between 20 and 30 then print jerry for even no. and tom for odd no.
+//else wrong input
+
 package com.stackroute.pe1;
 
 import java.util.Scanner;
@@ -5,7 +8,7 @@ public class tom_and_jeery {
     public static void main(String[] args) {
         System.out.print("enter the number=");
         Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
+        int number = sc.nextInt();//input integer
         System.out.println(tom_and_jerry_check(number));
     }
     public static String tom_and_jerry_check(int number)
