7794056ccaf806c5250e5f1e7e4234535bfb18ac
ua007 <35341797+ua007@users.noreply.github.com>
2019-07-03 17:22:39 +0530

    Update patternTest.java

--- src/main/test/com/stackroute/pe1/patternTest.java
+++ src/main/test/com/stackroute/pe1/patternTest.java
@@ -4,10 +4,21 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class patternTest {
+    pattern num;
+    @Before
+    public void setUp() throws Exception
+    {
+        num=new pattern();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        num=null;
+    }
     //test case for integer 5
     @Test
     public void givenIntegerShouldReturnPattern(){
-        pattern num=new pattern();
+        
         String result;
         result=num.printing_pattern(5);
         assertEquals("122333444455555",result);
