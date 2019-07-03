395fd308cc61703b05a810fd776f017a6a6e3210
ua007 <35341797+ua007@users.noreply.github.com>
2019-07-03 17:18:14 +0530

    Update GuessTheNumberTest.java

--- src/main/test/com/stackroute/pe1/GuessTheNumberTest.java
+++ src/main/test/com/stackroute/pe1/GuessTheNumberTest.java
@@ -1,27 +1,39 @@
 package com.stackroute.pe1;
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class GuessTheNumberTest {
+    
+    @Before
+    public void setUp() throws Exception
+    {
+        ob=new GuessTheNumber();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        ob=null;
+    }
     @Test
     public void givenIntegerIsEqualToRandomNumberShouldReturnMessage(){
-        GuessTheNumber num=new GuessTheNumber();
+       
         String result;
         result=num.guessTheNumber(45,45);
         assertEquals("Number guessed matches the original number",result);
     }
     @Test
     public void givenIntegerIsGreaterThanRandomNumberShouldReturnMessage(){
-        GuessTheNumber num=new GuessTheNumber();
+        
         String result;
         result=num.guessTheNumber(55,45);
         assertEquals("Number guessed is more than original number",result);
     }
     @Test
     public void givenIntegerIsLessThanRandomNumberShouldReturnMessage(){
-        GuessTheNumber num=new GuessTheNumber();
+        
         String result;
         result=num.guessTheNumber(45,55);
         assertEquals("Number guessed is less than original number",result);
