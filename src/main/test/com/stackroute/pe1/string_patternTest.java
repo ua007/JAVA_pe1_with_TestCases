e8043b4caeca8335d75175d149c88f7a2de261f8
ua007 <35341797+ua007@users.noreply.github.com>
2019-07-03 17:27:21 +0530

    Update string_patternTest.java

--- src/main/test/com/stackroute/pe1/string_patternTest.java
+++ src/main/test/com/stackroute/pe1/string_patternTest.java
@@ -1,9 +1,23 @@
 package com.stackroute.pe1;
-
+import org.junit.After;
+import org.junit.Before;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class string_patternTest {
+    
+    string_pattern str;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        str=new string_pattern();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        str=null;
+    }
     //test case for string stackroute and integer 2
     @Test
     public void givenStringShouldReturnStringPattern(){
